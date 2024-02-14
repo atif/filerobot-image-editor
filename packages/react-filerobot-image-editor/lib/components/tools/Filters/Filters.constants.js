a83e370e6d41674a512609aaee538d0b1f9ba994
Atif Dastgir <atif.dastgir@gmail.com>
2024-02-14 12:52:29 +0500

    Add lib folder

---
+++ packages/react-filerobot-image-editor/lib/components/tools/Filters/Filters.constants.js
@@ -0,0 +1,1 @@
+import Konva from"konva";import*as CustomKonvaFilters from"../../../custom/filters";var konvaFilters=Konva.Filters;export var AVAILABLE_FILTERS=[{label:"Original",filterFn:null},{label:"Black & White",filterFn:CustomKonvaFilters.Moon}];
